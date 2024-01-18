99010c925d09a639f99ce2db80d268d49c7dff9e
iMac <>
2024-01-18 14:32:35 +0530

    gradle issue solved

--- app/src/main/java/com/oneflow/analytics/utils/OFConstants.java
+++ app/src/main/java/com/oneflow/analytics/utils/OFConstants.java
@@ -20,7 +20,7 @@ package com.oneflow.analytics.utils;
 
 public interface OFConstants {
 
-    String currentVersion = "2023.09.31";//2023.07.14;
+    String currentVersion = "2023.09.32";//2023.07.14;
     String MODE = "dev";//"beta";//"prod";//
 
     String PLATFORM = "Android";
