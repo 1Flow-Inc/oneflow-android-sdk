884bcde91e278db274049a793454a1a0e428123d
imac <>
2024-08-23 17:59:41 +0530

    crash issue resolved

--- app/src/main/java/com/oneflow/analytics/utils/OFConstants.java
+++ app/src/main/java/com/oneflow/analytics/utils/OFConstants.java
@@ -20,7 +20,7 @@ package com.oneflow.analytics.utils;
 
 public interface OFConstants {
 
-    String currentVersion = "2024.06.23";//2023.07.14;
+    String currentVersion = "2024.08.23";//2023.07.14;
     String MODE = "prod";//"dev";//"beta";//
 
     String PLATFORM = "Android";
